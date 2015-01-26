3b3547ac8434757019d258348f2741032bfcd63f
Dean Barnett <dean@shopkeep.com>
2015-01-26 08:37:30 +0000

    add spotify client with search track method

--- lib/sonos_cli/spotify_client.rb
+++ lib/sonos_cli/spotify_client.rb
@@ -1,14 +1,28 @@
 require 'spotify_client'
 
 class SpotifyClient
-  attr_reader :client
+  RESULT_SIZE = 5
 
   def initialize
     @client = Spotify::Client.new
   end
 
-  def search_track
-    # client.search(:track. 'pulling teeth')
-    # fetch('items')[1].fetch('id')
+  def search_track(search_string)
+    results_for(search_string).map do |result|
+      {
+        id: result.fetch('id'),
+        artist: result.fetch('artists').first['name'],
+        track: result.fetch('name')
+      }
+    end
+  end
+
+  private
+
+  def results_for(search_string)
+    @client.search(:track, search_string)
+           .fetch('tracks')
+           .fetch('items')
+           .first(RESULT_SIZE)
   end
 end
